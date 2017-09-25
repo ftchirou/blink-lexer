f43b173c714729e5250d941c9df2184fbaa46700
Danielo Rodriguez Rivero <rdanielo@gmail.com>
2017-09-25 19:52:41 +0200

    feat(util): Created the Dict class that allows both key and value lookups in an object

---
+++ src/main/lexer/Dict.js
@@ -0,0 +1,34 @@
+import { fromArrays } from './objectUtils';
+
+export default class Dict {
+    constructor(obj) {
+        this.map = obj;
+        this._keys = Object.keys(obj);
+        this._values = Object.values(obj)
+        this.reversed = fromArrays(this._values, this._keys)
+    }
+
+    getKeyFor(value) {
+        return this.reversed[value]
+    }
+
+    getValue(key) {
+        return this.map[key]
+    }
+
+    hasValue(value) {
+        return !!this.getKeyFor(value)
+    }
+
+    get keys() {
+        return this._keys
+    }
+
+    get values() {
+        return this._values
+    }
+
+    get object() {
+        return this.map
+    }
+}
